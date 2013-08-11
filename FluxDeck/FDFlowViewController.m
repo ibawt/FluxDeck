0fb693384503332334afcac535cd6d5e63d44f49
Ian Quick <ian.quick@gmail.com>
2013-08-11 12:48:03 -0400

    variable heights

--- FluxDeck/FDFlowViewController.m
+++ FluxDeck/FDFlowViewController.m
@@ -30,11 +30,12 @@
     return self;
 }
 
+
 -(NSAttributedString*)parseMessageContent:(NSString*)str
 {
 	NSError *error = nil;
 
-	NSDataDetector *linkDetector = [NSDataDetector dataDetectorWithTypes:NSTextCheckingTypeLink error:&error];
+	NSDataDetector *linkDetector = [NSDataDetector dataDetectorWithTypes:(NSTextCheckingTypes)NSTextCheckingTypeLink error:&error];
 
 	NSArray *matches = [linkDetector matchesInString:str options:0 range:NSMakeRange(0, str.length)];
 	NSMutableAttributedString *attrString = [[NSMutableAttributedString alloc] initWithString:str];
@@ -55,7 +56,8 @@
 	_flow = flow;
 	[self fetchMessages];
 	[self populateUsers];
-
+	[self.chatTableView setDoubleAction:@selector(doubleClicked)];
+	
 	self.requestStream = [FDRequest initWithString:[NSString stringWithFormat:@"%@/messages", self.flow.url] withBlock:^(NSObject *object, NSError *error){
 		
 	} forStreaming:YES ];
@@ -70,6 +72,25 @@
 {
 
 }
+/*
+-(BOOL)tableView:(NSTableView *)tableView shouldSelectRow:(NSInteger)row
+{
+	NSInteger selected = row;
+
+    // Get row at specified index
+    NSTableCellView *selectedRow = [tableView viewAtColumn:0 row:selected makeIfNecessary:YES];
+
+    // Get row's text field
+    NSTextField *selectedRowTextField = [selectedRow textField];
+
+    // Focus on text field to make it auto-editable
+    [[self.view window] makeFirstResponder:selectedRowTextField];
+
+    // Set the keyboard carat to the beginning of the text field
+    [[selectedRowTextField currentEditor] setSelectedRange:NSMakeRange(0, 0)];
+	return YES;
+}
+ */
 
 - (NSInteger)numberOfRowsInTableView:(NSTableView *)aTableView
 {
@@ -85,6 +106,16 @@
 	NSAssert(true, @"Invalid table view sent to controller");
 	return -1;
 }
+- (BOOL)tableView:(NSTableView *)aTableView
+shouldEditTableColumn:(NSTableColumn *)aTableColumn row:(NSInteger)rowIndex
+{
+	return NO;
+}
+
+-(IBAction)doubleClicked:(id)sender
+{
+	;
+}
 
 - (NSView *)tableView:(NSTableView *)tableView viewForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
 	if( tableView == self.chatTableView ) {
@@ -97,9 +128,7 @@
 		{
 			FDMessage *msg = [self.messages objectAtIndex:row];
 			cellView.textField.attributedStringValue = [self parseMessageContent:[NSString stringWithFormat:@"%@: %@", FDGetUserFromID(msg.user).nick, msg.content]];
-			
-			cellView.toolTip = FDGetUserFromID(msg.user).name;
-			
+			[cellView.textField setSelectable:YES];
 			return cellView;
 		}
 		return cellView;
@@ -109,6 +138,9 @@
 		NSString *userID = [self.flow.users objectAtIndex:row];
 		FDUser *user = [users objectForKey:userID];
 		cell.textField.stringValue = user.nick;
+
+		cell.toolTip = user.name;
+
 		[FDImageCache getDataForURL:user.avatar onComplete:^(NSData *data, NSError *error){
 			cell.imageView.image = [[NSImage alloc] initWithData:data];
 		}];
@@ -117,6 +149,16 @@
 
 	}
 	return nil;
+}
+
+-(CGFloat)tableView:(NSTableView *)tableView heightOfRow:(NSInteger)row
+{
+	if( tableView == self.chatTableView ) {
+		NSString *c = [[self.messages objectAtIndex:row] content];
+		NSRect rect = [c boundingRectWithSize:NSMakeSize(self.chatTableView.frame.size.width, 0) options:NSStringDrawingUsesLineFragmentOrigin attributes:nil];
+		return MAX( rect.size.height, 40);
+	}
+	return 40;
 }
 
 -(void)fetchMessages
